36bbfe837df1d7cc863ff6621b2a05a47ed431cf
Prashant Garg <prashant.garg.ind@gmail.com>
2025-02-04 12:45:44 +0530

    events update

--- components/calender/calendar.tsx
+++ components/calender/calendar.tsx
@@ -7,12 +7,9 @@ import NextImage from "next/image";
 
 const calendarData = {
     entries: [
-        // { date: "2024-11-26", label: "Utpanna Ekadashi" },
-        // { date: "2024-11-26", label: "Disappearance Day: Srila Narahari Sarkar Thakur" },
-        // { date: "2024-11-27", label: "Disappearance Day: Srila Kaliya Krishna Das" },
-        // { date: "2024-11-28", label: "Disappearance Day: Srila Sarang Thakur" },
-        // { date: "2024-12-07", label: "Udann Shashthi" },
-        { date: "2024-12-26", label: "Saphala Ekadashi" },
+        { date: "2025-02-08", label: "Ekadashi" },
+        { date: "2025-02-09", label: "Appearance Day: Lord Varaha" },
+        { date: "2025-02-10", label: "Appearance Day: Lord Nityanand" },
     ],
     title: "DATE DETAILS"
 }
